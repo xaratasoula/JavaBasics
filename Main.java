55426faa27d04901e8d54579bbb18a8f773fc063
xaratasoula <36745255+xaratasoula@users.noreply.github.com>
2021-04-02 17:45:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package mainpackage;
+public class Main{
+    public static void main(String[] args) {
+        Users User1 = new Users();
+        User1.login();
+        Patient Patient = new Patient();
+        Patient.get_set();
+        Patient.Registration();
+
+
+    }
+}

11a1ff6066a82d2dfd7354237d93fff13e0f70f3
Arisgitsas <>
2021-04-03 19:36:46 +0300

    Completed Doctor and Patient class

---
+++ 4ο Εξάμηνο/JavaBasics/src/mainpackage/Appointment.java
@@ -0,0 +1,6 @@
+package mainpackage;
+
+public class Appointment extends Patient {
+
+
+}
--- 4ο Εξάμηνο/JavaBasics/src/mainpackage/Patient.java
+++ 4ο Εξάμηνο/JavaBasics/src/mainpackage/Patient.java
@@ -2,6 +2,36 @@ package mainpackage;
 import java.util.Scanner;
 
 public class Patient extends Users{
+    public String meetDate;
+    public String Day;
+    public String time;
+    public int MeetingId;
+    public static String amka;
+    public String getMeetDate() {
+        return meetDate;
+    }
+    public void setName(String newmeetDate) {
+        this.meetDate=newmeetDate;
+    }
+    public String getDay() {
+        return Day;
+    }
+    public void setDay(String newDay) {
+        this.Day=newDay;
+    }
+    public String getTime() {
+        return time;
+    }
+    public void setSurname(String newtime) {
+        this.time=newtime;
+    }
+    public Integer getMeetingId() {
+        return MeetingId;
+    }
+    public void setPassword(Integer newMeetingId) {
+        this.MeetingId=newMeetingId;
+    }
+
     void Users(){
         username="";
         name="name1";
@@ -9,7 +39,7 @@ public class Patient extends Users{
         password="pass1";
         amka="2345";
     };
-    public static String amka;
+
     public void get_set() {
         Scanner obj3 = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter your AMKA");
@@ -33,6 +63,30 @@ public class Patient extends Users{
             String password = obj4.nextLine(); // Read user input
             System.out.println("Your pass is: " + password);  // Output user input
         }
+
+    }
+    public void ArrangeMeeting(){
+        meetDate = "12/02/2021";
+        Day = "Thursday";
+        time = "13:30";
+        MeetingId = 0;
+    }
+    public void SearchMeeting(){
+        Scanner objmeet = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter your MeetingId");
+
+        String amka = objmeet.nextLine();  // Read user input
+        System.out.println("Your AMKA is: " + MeetingId);  // Output user input
+        this.MeetingId = MeetingId;
+
+    }
+    public void PatientHistory(){
+        Scanner objamka = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter your AMKA to find your patient history:");
+
+        String amka = objamka.nextLine();  // Read user input
+        System.out.println("Your AMKA is: " + amka);  // Output user input
+        this.amka = amka;
 
     }
 }
